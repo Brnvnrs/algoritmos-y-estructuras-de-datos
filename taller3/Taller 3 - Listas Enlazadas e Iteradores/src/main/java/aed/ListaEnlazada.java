a8f51bc2b7cd25ec3d7e53d5569e7955c6015966
brian <ivan.rios2010@gmail.com>
2025-10-06 02:39:13 -0300

    hay que corregir le metodo agregar atras y ver que metodo no esta funcionando segun el test

--- taller3/Taller 3 - Listas Enlazadas e Iteradores/src/main/java/aed/ListaEnlazada.java
+++ taller3/Taller 3 - Listas Enlazadas e Iteradores/src/main/java/aed/ListaEnlazada.java
@@ -2,29 +2,56 @@ package aed;
 
 public class ListaEnlazada<T> {
     // Completar atributos privados
-
+    private Nodo nodoPrimero;
+    private Nodo nodoUltimo;
+    private int longitud;
     private class Nodo {
         // Completar
+        private T valor;
+        private Nodo nodoSiguiente;
+        private Nodo nodoAnterior;
+        public Nodo(T valor){
+            this.valor = valor;
+            this.nodoSiguiente = null;
+            this.nodoAnterior = null;
+        }
     }
 
     public ListaEnlazada() {
-        throw new UnsupportedOperationException("No implementada aun");
+        this.nodoPrimero = null;
+        this.nodoUltimo = null;
+        this.longitud = 0;
     }
 
     public int longitud() {
-        throw new UnsupportedOperationException("No implementada aun");
+        return this.longitud;
     }
 
     public void agregarAdelante(T elem) {
-        throw new UnsupportedOperationException("No implementada aun");
+        if(this.nodoPrimero.valor == null){
+            this.nodoPrimero.valor = elem;
+        }
+        else{
+            Nodo nodoNuevo = new Nodo(elem);
+            nodoNuevo.nodoSiguiente = nodoPrimero;
+            nodoPrimero = nodoNuevo;
+        }
+        this.longitud += 1;
     }
 
     public void agregarAtras(T elem) {
-        throw new UnsupportedOperationException("No implementada aun");
+        while(this.nodoPrimero != null){
+            nodoPrimero = nodoPrimero.nodoSiguiente;
+        }
+        
     }
 
     public T obtener(int i) {
-        throw new UnsupportedOperationException("No implementada aun");
+        int j = 0;
+        while(j<i){
+            nodoPrimero = nodoPrimero.nodoSiguiente;
+        }
+        return nodoPrimero.valor;
     }
 
     public void eliminar(int i) {
